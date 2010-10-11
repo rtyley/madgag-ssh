8806181056999a993ab34020bc16500b964dea8c
roberto <roberto.tyley@guardian.co.uk>
2010-10-11 12:33:40 +0100

    Populated the plain-flavour (non-android) AuthAgent interface

--- ssh/src/main/java/com/madgag/ssh/authagent/AuthAgent.java
+++ ssh/src/main/java/com/madgag/ssh/authagent/AuthAgent.java
@@ -1,5 +1,9 @@
 package com.madgag.ssh.authagent;
 
+import java.util.Map;
+
 public interface AuthAgent {
-	
+	Map<String,byte[]> getIdentities();
+
+	byte[] sign(byte[] publicKey, byte[] data);
 }
